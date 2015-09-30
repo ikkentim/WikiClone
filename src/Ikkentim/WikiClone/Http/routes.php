2b72f9e6be8b052a7d21a09bce81b405f4cb3578
Tim Potze <timpotze@gmail.com>
2015-09-30 11:32:20 -0400

    Updated composer.json

--- src/Ikkentim/WikiClone/Http/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::post('/', '\Ikkentim\WikiClone\Http\Controllers\WebhookController@trigger');
-Route::get('/{page?}', '\Ikkentim\WikiClone\Http\Controllers\DocumentationController@index')
-    ->where('page', '(.*)');
